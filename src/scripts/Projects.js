b35e689716665af89708239b4aa78b98bceec6df
Jaison Justus <jaison.justus.lp@gmail.com>
2015-12-18 16:57:54 +0530

    modified the Project listing page with respect to the design

--- src/scripts/Projects.js
+++ src/scripts/Projects.js
@@ -17,10 +17,6 @@ export default class Projects extends React.Component {
     super();
   };
 
-  onProjectItemSelect = (id, name) => {
-    this.props.onProjectSelect(id, name);
-  };
-
   renderProjectList = () => {
     return this.props.projects.map((project, idx) => {
       return (
@@ -39,13 +35,26 @@ export default class Projects extends React.Component {
     this.props.addNewProject(name);
   };
 
+  onManage = (id, name) => {
+    this.props.onProjectSelect(id, name);
+  };
+
   render()  {
     return (
-      <div id="Projects">
-        <ol className="Projects__List">
-          <ProjectItem/>
+      <section className="Wrapper--Projects">
+        <ol className="Projects">
+          <ProjectItem id={1} name={"Vicious Player"} onSelect={this.onManage}/>
+          <ProjectItem id={2} name={"KTM.JS Website"} onSelect={this.onManage}/>
+          <ProjectItem id={3} name={"Samagri Toolkit"} onSelect={this.onManage}/>
         </ol>
-      </div>
+        <footer className="Footer">
+          <div className="Footer__Hills"></div>
+          <div className="Footer__Gate"></div>
+          <div className="Footer__Tree"></div>
+          <div className="Footer__Birds"></div>
+          <div className="Footer__Birds--small"></div>
+        </footer>
+      </section>
     );
   }
 };
@@ -71,22 +80,25 @@ class ProjectItem extends React.Component {
     return (
       <li className="Projects__List-Item Project">
         
-        <div className="Project__Wrapper">
-          <h2 className="Project__Name">Vicious Player</h2>
+        <div className="Project__Wrapper Project__Wrapper--name">
+          <h2 className="Project__Name">{this.props.name}</h2>
         </div>
         
-        <div className="Project__Wrapper">
-          <p className="Project__Label">spend</p>
-          <p className="Project__Time-Spend">12hrs 05m</p>
+        <div className="Project__Wrapper Project__Wrapper--time-spend">
+          <p className="Label Project__Label">spend</p>
+          <p className="Project__Time-Spend">
+            12<span className="Project__Time-Spend--param">hrs</span> 
+            05<span className="Project__Time-Spend--param">m</span>
+          </p>
         </div>
 
-        <div className="Project__Wrapper">
-          <p className="Project__Label">this week focus</p>
+        <div className="Project__Wrapper Project__Wrapper--focus">
+          <p className="Label Project__Label">this week focus</p>
           <p className="Project__Focus">Create an UI for collecting music info and persist it in RethinkDB</p>
         </div>
 
-        <div className="Project__Wrapper">
-          <button className="Project__Manage Button">Manage</button>
+        <div className="Project__Wrapper Project__Wrapper--button">
+          <button className="Project__Manage Button" onClick={this.onItemSelect}>Manage</button>
         </div>
       </li>
     )
